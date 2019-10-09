fad541c3ccef4c07fde38c7866e19021e8ca3ce3
Frank Louwers <frank@louwers.be>
2019-10-09 10:58:47 +0200

    Initial import - beid version 4.4.20 (supports Catalina)

---
+++ Casks/eid-be.rb
@@ -0,0 +1,18 @@
+cask 'eid-be' do
+  version '4.4.20'
+  sha256 '72a32872accc12596a05f085ef58e2ec86c7053352328bcbaf1bf1a1db3ccbf4'
+
+  url "https://eid.belgium.be/sites/default/files/software/eid-quickinstaller-#{version}.dmg"
+  name 'Electronic identity card software of Belgium'
+  name 'eID Belgium Quickinstaller'
+  homepage 'https://eid.belgium.be/'
+
+  pkg 'eID-Quickinstaller-signed.pkg'
+
+  uninstall pkgutil: 'be.eid.*'
+
+  caveats do
+    depends_on_java
+  end
+end
+
