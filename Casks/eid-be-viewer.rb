4043d8a67d3ab33b887858da96974288cdc5b826
Frank Louwers <frank@louwers.be>
2021-06-26 00:07:28 +0200

    update url

--- Casks/eid-be-viewer.rb
+++ Casks/eid-be-viewer.rb
@@ -2,7 +2,7 @@ cask 'eid-be-viewer' do
   version '5.0.24'
   sha256 'dcc2d9c421372e82444e25938d70fc624b0594e156fc0968135d69a0fcf2a026'
 
-  url "https://eid.belgium.be/sites/default/files/software/eID%20viewer-#{version}.dmg"
+  url "https://eid.belgium.be/sites/default/files/software/eID%20Viewer-#{version}.dmg"
   name 'Electronic identity card Viewer for Belgium'
   homepage 'https://eid.belgium.be/'
 
