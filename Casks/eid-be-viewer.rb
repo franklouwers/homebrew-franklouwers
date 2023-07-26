a45068e5751195936009c98e6f50cf307a8c563a
Bart Verhoeven <bartverhoeven@live.com>
2023-07-26 14:07:09 +0800

    Update eid-be-viewer.rb
    Update eid-be-viewer.rb

--- Casks/eid-be-viewer.rb
+++ Casks/eid-be-viewer.rb
@@ -1,6 +1,6 @@
 cask 'eid-be-viewer' do
-  version '5.0.26'
-  sha256 '4ca9510c72708e149f24de4eabb0459ade9e06c28f85cdd3014ac9df1a179a01'
+  version '5.1.3'
+  sha256 '611d8d7708afb842217f73bf75a1798152b42c287600909ee7144598c42a2014'
 
   url "https://eid.belgium.be/sites/default/files/software/eID%20Viewer-#{version}.dmg"
   name 'Electronic identity card Viewer for Belgium'
